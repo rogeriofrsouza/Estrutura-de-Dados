565182cb669048238e1b3067463cf9779a4a990b
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2022-10-06 16:26:29 -0300

    Ajustes pilhas e filas

--- pratica/filas/fila.c
+++ pratica/filas/fila.c
@@ -1,17 +1,19 @@
 /*
-  O  programa  a  seguir  ilustra  a  implementação  completa  de  uma  fila  de  números  inteiros representada  por  encadeamento  em  
-  linguagem  C. Para  testá-lo,  adapte-o  para  imprimir  o conteúdo da fila antes e após a exclusão.
+  O programa a seguir ilustra a implementação completa de uma fila de números inteiros representada por encadeamento em 
+  linguagem C. Para testá-lo, adapte-o para imprimir o conteúdo da fila antes e após a exclusão.
 */
 #include <stdlib.h>
 #include <stdio.h>
 
-struct regLista {
+struct regLista 
+{
   int valor;
   struct regLista *prox;
 };
 typedef struct regLista TLista;
 
-struct descrFila {
+struct descrFila 
+{
   TLista *inicio;
   TLista *final;
   int qtde;
@@ -20,14 +22,14 @@ typedef struct descrFila DFila;
 
 int main(void)
 {
-  int numero;
-  DFila descritor;
   TLista *aux;
+  DFila descr;
+  int numero;
 
   /* inicializando o descritor da fila */
-  descritor.inicio = NULL;
-  descritor.final  = NULL;
-  descritor.qtde = 0;
+   descr.inicio = NULL;
+   descr.final = NULL;
+   descr.qtde = 0;
   
   while (1)
   {
@@ -40,47 +42,53 @@ int main(void)
     /* alocando a struct TLista dinamicamente */
     aux = (TLista *) malloc(sizeof(TLista));
 
-    /* preenchendo os valores da struct alocada dinamicamente */
+    /* preenchendo os valores da struct alocada */
     aux->valor = numero;
     aux->prox = NULL;
     
-    /* cuidando do encadeamento do novo nó na lista */
-    if (descritor.inicio == NULL)
-      descritor.inicio = aux;
-    else 
-      descritor.final->prox = aux;
+    /* cuidando do encadeamento do novo nó na fila */
+    if (descr.inicio == NULL)
+      descr.inicio = aux;
+    else
+      descr.final->prox = aux;
     
     /* atualizando os descritores */
-    descritor.final = aux;
-    descritor.qtde++;
+    descr.final = aux;
+    descr.qtde++;
   }
   
   while (1)
   {
-    printf("\n\nDigite 1 para excluir ou outra coisa para encerrar:\n");
+    /* imprime fila */
+    printf("\n\nConteúdo da fila:\n");
+
+    for (aux = descr.inicio; aux != NULL; aux = aux->prox)
+      printf("%d\n", aux->valor);
+
+    printf("\nDigite 1 para excluir ou outra coisa para encerrar:\n");
     scanf("%d", &numero);
     
-    if (numero == 1)
+    if (numero != 1)
+      break;
+    
+    /* verifica se a fila está vazia */
+    if (descr.inicio != NULL)
     {
-      if (descritor.inicio != NULL)
-      {
-        aux = descritor.inicio;
-
-        descritor.inicio = descritor.inicio->prox;
-        descritor.qtde--;
-        
-        if (descritor.qtde == 0)
-          descritor.final = NULL;
-        
-        /* excluindo um valor da fila */
-        printf("\n\nExcluindo o valor %d da lista\n", aux->valor);
-        free(aux);
-      }
-      else
-      {
-        printf("\n\nA lista está vazia\n");
-        break;
-      }
+      aux = descr.inicio;
+      descr.inicio = descr.inicio->prox;
+      descr.qtde--;
+      
+      if (descr.inicio == NULL)
+        descr.final = NULL;
+      
+      /* excluindo um valor da fila */
+      printf("\nExcluindo o valor %d da lista\n", aux->valor);
+      free(aux);
+    }
+    else
+    {
+      printf("\nA lista está vazia\n");
+      break;
     }
   }
   
--- pratica/pilhas/pilha.c
+++ pratica/pilhas/pilha.c
@@ -1,16 +1,19 @@
 /* 
-  O  programa  apresentado  a  seguir  implementa  uma  pilha  básica  de  números  inteiros  em linguagem C. Para testá-lo, adapte-o para imprimir o conteúdo da pilha antes e após a exclusão.
+  O programa apresentado a seguir implementa uma pilha básica de números inteiros em linguagem C. 
+  Para testá-lo, adapte-o para imprimir o conteúdo da pilha antes e após a exclusão.
 */
 #include <stdio.h>
 #include <stdlib.h>
 
-struct regLista {
+struct regLista 
+{
   int valor;
   struct regLista *prox;
 };
 typedef struct regLista TLista;
 
-struct descrPilha {
+struct descrPilha 
+{
   TLista *topo;
   int qtde;
 };
@@ -18,57 +21,63 @@ typedef struct descrPilha DPilha;
 
 int main(void)
 {
-  int numero;
-  DPilha descritor;
   TLista *aux;
+  DPilha descr;
+  int num;
 
   /* inicializando o descritor da pilha */
-  descritor.topo = NULL;
-  descritor.qtde = 0;
+  descr.topo = NULL;
+  descr.qtde = 0;
   
   while (1)
   {
     printf("Informe o número:\n");
-    scanf("%d", &numero);
+    scanf("%d", &num);
     
-    if (numero < 0)
+    if (num < 0)
       break;
     
     /* alocando a struct TLista dinamicamente */
     aux = (TLista *) malloc(sizeof(TLista));
 
-    /* preenchendo os valores da struct alocada dinamicamente */
-    aux->valor = numero;
-    aux->prox = descritor.topo;
+    /* preenchendo os valores da struct alocada */
+    aux->valor = num;
+    aux->prox = descr.topo;
 
-    /* atualizando os descritores */
-    descritor.topo = aux;
-    descritor.qtde++;
+    /* atualizando o descritor */
+    descr.topo = aux;
+    descr.qtde++;
   }
-   
+
   while (1)
   {
-    printf("\n\nDigite 1 para excluir ou outra coisa para encerrar:\n");
-    scanf("%d", &numero);
+    /* imprime pilha */
+    printf("\n\nConteúdo da pilha:\n");
 
-    if (numero == 1)
+    for (aux = descr.topo; aux != NULL; aux = aux->prox)
+      printf("%d\n", aux->valor);
+
+    printf("\nDigite 1 para excluir ou outra coisa para encerrar:\n");
+    scanf("%d", &num);
+
+    if (num != 1)
+      break;
+    
+    /* verifica se a pilha está vazia */
+    if (descr.topo != NULL)
     {
-      if (descritor.topo != NULL)
-      {
-        aux = descritor.topo;
-
-        descritor.topo = descritor.topo->prox;
-        descritor.qtde--;
-        
-        /* excluindo um valor da pilha */
-        printf("\n\nExcluindo o valor %d da pilha\n", aux->valor);
-        free(aux);
-      }
-      else
-      {
-        printf("\n\nA lista está vazia\n");
-        break;
-      }
+      aux = descr.topo;
+      descr.topo = descr.topo->prox;
+      descr.qtde--;
+      
+      /* excluindo um valor da pilha */
+      printf("\nExcluindo o valor %d da pilha\n", aux->valor);
+      free(aux);
+    }
+    else
+    {
+      printf("\nA lista está vazia\n");
+      break;
     }
   }
   
