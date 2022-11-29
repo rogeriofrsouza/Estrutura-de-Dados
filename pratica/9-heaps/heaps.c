490dc690bb22023863e6c0aa78f67829dd85eef0
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2022-11-29 20:11:25 -0300

    Ajustes Max-Heap

--- pratica/9-heaps/heaps.c
+++ pratica/9-heaps/heaps.c
@@ -1,8 +1,10 @@
 /* 
+  Programa de construção e impressão de uma árvore binária completa: Max-Heap.
   Lógica básica para montar o Max-Heap: considerar que os valores informados foram armazenados no vetor na ordem indicada no enunciado. 
-  Executar então os passos a seguir para cada elemento do vetor que contém a árvore binária completa, começando do primeiro: 
+  Executar então os passos para cada elemento do vetor.
 */
 #include <stdio.h>
+#include <math.h>
 
 #define MAX 50
 
@@ -11,34 +13,65 @@ typedef int TNo;
 int main(void)
 {
   TNo vet[MAX];
-  int i, final = 0, m, f, aux;
+  int final, m, f, aux, qtde, nivel, idx;
 
-  printf("Informe os valores do vetor:\n");
+  printf("Informe os valores:\n");
 
-  /* seja V o vetor que contém a árvore completa (com elementos de 1 a N) */
-  for (i = 0; i < MAX; i++)
+  for (final = 1; final < MAX; final++)
   {
-    scanf("%d\n", vet[i]);
-    final++;
+    scanf("%d", &vet[final]);
+
+    if (vet[final] < 1)
+      break;
   }
 
-  /* faça M ser o índice do elemento a ser processado (considere sempre os índices começando em 1) */
-  m = 0;
+  /* índice do elemento a ser processado (considere sempre os índices começando em 1) */
+  m = 1;
 
-  /* faça F = M + 1 */
-  f = m + 1;
-
-  while (f > 1 && vet[f / 2] < vet[f])
+  while (1)
   {
-    /* trocando os elementos V[F / 2] e V[F] de lugar */
-    aux = vet[f / 2];
-    vet[f / 2] = vet[f];
-    vet[f] = aux;
+    f = m + 1;
 
-    /* cortando o valor de f pela metade e incrementando m*/
-    f /= 2;
+    if (f == final)
+      break;
+
+    while (f > 1 && vet[f/2] < vet[f])  /* f = nó, f/2 = ancestral */
+    {
+      /* trocando os elementos vet[f/2] e vet[f] de lugar */
+      aux = vet[f/2];
+      vet[f/2] = vet[f];
+      vet[f] = aux;
+
+      /* cortando o valor de f pela metade */
+      f /= 2;
+    }
+    
     m++;
   }
 
+  /* imprimindo Max-Heap */
+  printf("\n\n");
+
+  qtde = 0;
+  nivel = 0;
+  idx = 1;
+
+  while (1)
+  {
+    qtde += (int) pow(2, (double) nivel);
+
+    while (idx <= qtde && idx < final)
+    {
+      printf("%d ", vet[idx]);
+      idx++;
+    }
+    
+    if (idx == final)
+      break;
+
+    printf("\n");
+    nivel++;
+  };
+  
   return 0;
 }
